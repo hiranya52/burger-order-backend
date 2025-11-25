cb459dbb47454b04372fe192a0bdcd4c4a753c37
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-11-26 00:47:03 +0530

    Add Product Entity class

--- src/main/java/edu/icet/model/entity/Product.java
+++ src/main/java/edu/icet/model/entity/Product.java
@@ -1,4 +1,21 @@
 package edu.icet.model.entity;
 
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Product {
+
+    private String productCode;
+    private String name;
+    private String category;
+    private double price;
+    private int qtyOnHand;
+
 }
